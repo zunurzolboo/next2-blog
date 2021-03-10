9acddc632e7fa1c205dd15f5f77e9310f16cea75
zunurzolboo <unurzolboo@gmail.com>
2021-03-10 11:32:42 +0800

    react-bootstrap holbold hiij alerrtt ajjiluul aa

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,23 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-
+import Alert from "react-bootstrap/Alert";
 export default function Home() {
-  return <div className={styles.container}>\ My blog</div>;
+  return (
+    <div className={styles.container}>
+      {[
+        "primary",
+        "secondary",
+        "success",
+        "danger",
+        "warning",
+        "info",
+        "light",
+        "dark",
+      ].map((variant, idx) => (
+        <Alert key={idx} variant={variant}>
+          This is a {variant} alert—check it out!
+        </Alert>
+      ))}
+    </div>
+  );
 }
